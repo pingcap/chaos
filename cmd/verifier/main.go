9addb7cf310350601c336c668ec159fd3f2d5f94
siddontang <siddontang@gmail.com>
2018-09-07 09:47:10 +0800

    add pprof support

--- cmd/verifier/main.go
+++ cmd/verifier/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"flag"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,6 +15,7 @@ import (
 var (
 	historyFile = flag.String("history", "./history.log", "history file")
 	names       = flag.String("names", "", "verifier names, seperate by comma")
+	pprofAddr   = flag.String("pprof", "0.0.0.0:6060", "Pprof address")
 )
 
 func main() {
@@ -21,6 +24,10 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		http.ListenAndServe(*pprofAddr, nil)
+	}()
 
 	go func() {
 		<-sigs
